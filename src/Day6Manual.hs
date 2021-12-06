ed7801d4dde52ff7770a5891d1898a724e1acaf0
Einar Strandberg <einargs@gmail.com>
2021-12-06 11:21:06 -0800

    Cleaned up manual day 6 implementation

--- src/Day6Manual.hs
+++ src/Day6Manual.hs
@@ -9,21 +9,20 @@ import qualified Data.Vector as V
 
 import Day
 
-parse :: Text -> [Word]
-parse = runParse $ word `P.sepBy` C.string "," where
-  word = fromIntegral <$> L.decimal
+parse :: Text -> [Int]
+parse = runParse $ L.decimal `P.sepBy` C.string ","
 
 -- | For a new fish with `days` left, gives the number of fish resulting.
-simNewFish :: (Word -> Word) -> Word -> Word
+simNewFish :: (Int -> Int) -> Int -> Int
 simNewFish f days = f' days 7 + f' days 9 where
   f' days n | days < n = 1
   f' days n = f (days - n)
 
-simulate :: Word -> [Word] -> Word
+simulate :: Int -> [Int] -> Int
 simulate startDays = sum . fmap simFish where
   simFish init = simNewFish' (startDays - 1 - init)
-  simNewFish' = simNewFish $ (lazyVec V.!) . fromIntegral
-  lazyVec = V.generate (fromIntegral startDays) (simNewFish' . fromIntegral)
+  simNewFish' = simNewFish (lazyVec V.!)
+  lazyVec = V.generate (startDays - 1) simNewFish'
 
 day6 :: Day
 day6 = Day (simFor 80) (simFor 256) (Just "351188") (Just "1595779846729") where
