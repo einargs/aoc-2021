dd6ed7d367be7865bd0f2f6213fb33ce6b67a8dd
Einar Strandberg <einargs@gmail.com>
2021-12-08 13:42:18 -0800

    Improved Day 8

--- src/Day8.hs
+++ src/Day8.hs
@@ -6,10 +6,11 @@ import qualified Data.Text as T
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as C
 import qualified Text.Megaparsec.Char.Lexer as L
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, fromJust)
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.List (find, permutations)
+import Control.Monad (guard)
 import Debug.Trace
 
 import Day
@@ -57,33 +58,46 @@ segments :: [Disp]
 segments = convDisplay <$> ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg",
   "abdefg", "acf", "abcdefg", "abcdfg"]
 
-type Key = M.Map Disp Int
-
-trueKey :: Key
-trueKey = M.fromList $ zip segments [0..]
+trueKey :: Disp -> Int
+trueKey d = m M.! d where
+  m = M.fromList $ zip segments [0..]
 
 segmentSet :: S.Set Disp
 segmentSet = S.fromList segments
+
+toSol :: [Seg] -> Seg -> Seg
+toSol xs x = xs !! segToIndex x
 
 checkSolution :: [Disp] -> (Seg -> Seg) -> Bool
 checkSolution ds f = segmentSet == S.fromList (fmap (S.map f) ds)
 
 allSolutions :: [Seg -> Seg]
-allSolutions = convert <$> permutations [A .. G] where
-  convert :: [Seg] -> Seg -> Seg
-  convert xs x = xs !! segToIndex x
+allSolutions = toSol <$> permutations [A .. G]
 
-deduceKey :: Entry -> Key
-deduceKey (Entry ins _) = key where
-  check = checkSolution ins
-  Just solution = find check allSolutions
-  convert disp = S.map solution disp
-  key = M.fromList $ fmap f ins where
-    f d = (d, trueKey M.! convert d)
+bruteDeriveSolution :: [Disp] -> Seg -> Seg
+bruteDeriveSolution ds = sol where
+  check = checkSolution ds
+  Just sol = find check allSolutions
+
+deriveSolution :: [Disp] -> Seg -> Seg
+deriveSolution ds = fromJust $ find (checkSolution ds) $ do
+  sol <- permutations [A .. G]
+  let sol' = toSol sol
+  eqGuard sol' 1 2
+  eqGuard sol' 4 4
+  eqGuard sol' 7 3
+  eqGuard sol' 8 7
+  pure sol'
+  where
+    -- Get the display with the given size
+    getDisp size = fromJust $ find ((==size) . S.size) ds
+    eqGuard sol trueIndex size = guard $
+      S.map sol (getDisp size) == (segments !! trueIndex)
 
 calculateOutput :: Entry -> Int
-calculateOutput e@(Entry _ out) = mkNum $ fmap (key M.!) out where
-  key = deduceKey e
+calculateOutput e@(Entry ins out) = mkNum $ key <$> out where
+  sol = deriveSolution ins
+  key = trueKey . S.map sol
   mkNum :: [Int] -> Int
   mkNum = read . (>>= show)
 
