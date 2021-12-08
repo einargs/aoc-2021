a3445fa20e996429a9db2695b518b076849a477c
Einar Strandberg <einargs@gmail.com>
2021-12-08 13:16:24 -0800

    Brute force on day 8 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Day8 (day8) where
 
 import Data.Text (Text)
@@ -8,13 +9,34 @@ import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Maybe (mapMaybe)
 import qualified Data.Map as M
 import qualified Data.Set as S
-import Data.List (find)
+import Data.List (find, permutations)
 import Debug.Trace
 
 import Day
 
-data Entry = Entry {sigIns :: [Text], sigOuts :: [Text]}
+data Seg = A | B | C | D | E | F | G
+  deriving (Eq, Ord, Show, Enum)
+
+type Disp = S.Set Seg
+
+data Entry = Entry {sigIns :: [Disp], sigOuts :: [Disp]}
   deriving Show
+
+convChar :: Char -> Seg
+convChar = \case
+  'a' -> A
+  'b' -> B
+  'c' -> C
+  'd' -> D
+  'e' -> E
+  'f' -> F
+  'g' -> G
+
+convDisplay :: String -> Disp
+convDisplay = S.fromList . fmap convChar
+
+segToIndex :: Seg -> Int
+segToIndex = fromEnum
 
 parse :: Text -> [Entry]
 parse = runParse $ entry `P.sepEndBy` C.newline where
@@ -23,32 +45,44 @@ parse = runParse $ entry `P.sepEndBy` C.newline where
     C.string "| "
     outs <- sig `P.sepBy` C.hspace
     pure $ Entry ins outs
-  sig = fmap T.pack $ P.some $ P.oneOf ['a'..'g']
+  sig = fmap convDisplay $ P.some $ P.oneOf ['a'..'g']
 
 -- | Count occurances of 1,4,7, and 8 in outputs. 1=2,4=4,7=3,8=7.
 part1 :: [Entry] -> Int
 part1 = sum . fmap f where
   f (Entry _ outs) = length $ filter g outs where
-    g t = T.length t `elem` [2,4,3,7]
+    g d = S.size d `elem` [2,4,3,7]
 
-segments :: [Text]
-segments = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg",
-  "acf", "abcdefg", "abcdfg"]
+segments :: [Disp]
+segments = convDisplay <$> ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg",
+  "abdefg", "acf", "abcdefg", "abcdfg"]
 
-type Key = M.Map Text Int
+type Key = M.Map Disp Int
 
 trueKey :: Key
 trueKey = M.fromList $ zip segments [0..]
 
+segmentSet :: S.Set Disp
+segmentSet = S.fromList segments
+
+checkSolution :: [Disp] -> (Seg -> Seg) -> Bool
+checkSolution ds f = segmentSet == S.fromList (fmap (S.map f) ds)
+
+allSolutions :: [Seg -> Seg]
+allSolutions = convert <$> permutations [A .. G] where
+  convert :: [Seg] -> Seg -> Seg
+  convert xs x = xs !! segToIndex x
+
 deduceKey :: Entry -> Key
-deduceKey (Entry ins _) = M.mapKeys (T.map encode) trueKey where
-  Just eight = find ((==7) . T.length) ins
-  encode :: Char -> Char
-  encode c = T.index eight i where
-    Just i = T.findIndex (==c) "abcdefg"
+deduceKey (Entry ins _) = key where
+  check = checkSolution ins
+  Just solution = find check allSolutions
+  convert disp = S.map solution disp
+  key = M.fromList $ fmap f ins where
+    f d = (d, trueKey M.! convert d)
 
 calculateOutput :: Entry -> Int
-calculateOutput e@(Entry _ out) = traceShow (key, e) $ mkNum $ fmap (key M.!) out where
+calculateOutput e@(Entry _ out) = mkNum $ fmap (key M.!) out where
   key = deduceKey e
   mkNum :: [Int] -> Int
   mkNum = read . (>>= show)
@@ -57,5 +91,5 @@ part2 :: [Entry] -> Int
 part2 = sum . fmap calculateOutput
 
 day8 :: Day
-day8 = Day (wrap part1) (wrap part2) Nothing Nothing where
+day8 = Day (wrap part1) (wrap part2) (Just "349") (Just "1070957") where
   wrap f = T.pack . show . f . parse
